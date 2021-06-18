7b320b24c843cb8bc2bd472156a0d5ab3ed17c27
tmp64 <tmp32x2@gmail.com>
2021-06-18 22:33:40 +0700

    Add debugging code

--- include/appfw/dbg.h
+++ include/appfw/dbg.h
@@ -1,13 +1,93 @@
 #ifndef APPFW_DBG_H
 #define APPFW_DBG_H
 #include <cassert>
+#include <string_view>
+#include <functional>
 #include <appfw/compiler.h>
 
 namespace appfw {
 
-// TODO: Replace with custom
-#define AFW_ASSERT(cond) assert(cond)
-#define AFW_ASSERT_MSG(cond, msg) assert(cond)
+enum class AssertAction
+{
+	Abort = 0, //!< Call abort()
+	Ignore,    //!< Ignore the assertion
+	Break,     //!< Break into the debugger if possible (ignore otherwise)
+};
+
+using AssertCallback = std::function<AssertAction(std::string_view cond, std::string_view msg,
+                                                  std::string_view file, int line)>;
+
+/**
+ * Returns whether a debugger is attached at the moment.
+ */
+bool isDebuggerAttached();
+
+/**
+ * Called if an assertion fails.
+ * @return whether to break into the debugger
+ */
+bool onAssertionFailed(std::string_view cond, std::string_view msg, std::string_view file,
+                               int line);
+
+/**
+ * Sets the callback that will be called on assertion failure.
+ */
+void setAssertCallback(const AssertCallback &cb);
+
+/**
+ * Checks whether the condition is true. If not, crashes the program.
+ * The check will be performed in both debug and relese builds.
+ * @param   cond    The condition to check
+ * @param   msg     Message if it fails
+ */
+#define AFW_ASSERT_REL_MSG(cond, msg)                                                              \
+    do {                                                                                           \
+        if (!(cond)) {                                                                             \
+            if (appfw::onAssertionFailed(#cond, (msg), __FILE__, __LINE__)) {                      \
+                AFW_DEBUG_BREAK();                                                                 \
+            }                                                                                      \
+        }                                                                                          \
+    } while (false)
+
+/**
+ * Checks whether the condition is true. If not, crashes the program.
+ * The check will be performed in both debug and relese builds.
+ * @param   cond    The condition to check
+ */
+#define AFW_ASSERT_REL(cond) AFW_ASSERT_REL_MSG(cond, "")
+
+#if AFW_DEBUG_BUILD
+
+/**
+ * Checks whether the condition is true. If not, crashes the program.
+ * The check will be performed only in debug builds.
+ * @param   cond    The condition to check
+ * @param   msg     Message if it fails
+ */
+#define AFW_ASSERT_MSG(cond, msg) AFW_ASSERT_REL_MSG(cond, msg)
+
+/**
+ * Checks whether the condition is true. If not, crashes the program.
+ * The check will be performed only in debug builds.
+ * @param   cond    The condition to check
+ */
+#define AFW_ASSERT(cond) AFW_ASSERT_REL(cond)
+
+#else
+
+/**
+ * Assertions are disabled in debug builds.
+ * Use AFW_ASSERT_REL_MSG instead.
+ */
+#define AFW_ASSERT_MSG(cond, msg) /* disabled */
+
+/**
+ * Assertions are disabled in debug builds.
+ * Use AFW_ASSERT_REL instead.
+ */
+#define AFW_ASSERT(cond) /* disabled */
+
+#endif // AFW_DEBUG_BUILD
 
 } // namespace appfw
 
--- src/dbg.cpp
+++ src/dbg.cpp
@@ -1,1 +1,71 @@
+#include <appfw/appfw.h>
 #include <appfw/dbg.h>
+#include <appfw/init.h>
+
+#if PLATFORM_WINDOWS
+#include <appfw/windows.h>
+#endif
+
+namespace {
+
+appfw::AssertCallback s_AssertCb;
+
+appfw::AssertAction defaultAssertCallback(std::string_view, std::string_view, std::string_view, int) {
+    if (appfw::isDebuggerAttached()) {
+        return appfw::AssertAction::Break;
+    } else {
+        return appfw::AssertAction::Abort;
+    }
+}
+
+}
+
+bool appfw::isDebuggerAttached() {
+#if PLATFORM_WINDOWS
+    return IsDebuggerPresent();
+#else
+    return false;
+#endif
+}
+
+bool appfw::onAssertionFailed(std::string_view cond, std::string_view msg, std::string_view file,
+                              int line) {
+    if (appfw::isInitialized()) {
+        printwtf("Assertion failed: {}", cond);
+        if (!msg.empty()) {
+            printwtf("Reason: {}", msg);
+        }
+        printwtf("File: {}:{}", file, line);
+    }
+
+    AssertAction action;
+
+    if (s_AssertCb) {
+        action = s_AssertCb(cond, msg, file, line);
+    } else {
+        action = defaultAssertCallback(cond, msg, file, line);
+    }
+
+    switch (action) {
+    case AssertAction::Abort: {
+        abort();
+        return false;
+    }
+    case AssertAction::Ignore: {
+        return false;
+    }
+    case AssertAction::Break: {
+        return true;
+    }
+    default: {
+        // assertception!
+        // AFW_ASSERT(false);
+        abort();
+        return false;
+    }
+    }
+}
+
+void appfw::setAssertCallback(const AssertCallback &cb) {
+    s_AssertCb = cb;
+}
