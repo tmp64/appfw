f52a0769e85bb28890f0b7b2d9e762e9518099d9
tmp64 <tmp32x2@gmail.com>
2022-01-06 11:03:21 +0700

    Windows: Check if macros are defined

--- include/appfw/windows.h
+++ include/appfw/windows.h
@@ -2,8 +2,14 @@
 #define APPFW_WINDOWS_H
 
 // Kill me
+#ifndef WIN32_LEAN_AND_MEAN
 #define WIN32_LEAN_AND_MEAN
+#endif
+
+#ifndef NOMINMAX
 #define NOMINMAX
+#endif
+
 #include <windows.h>
 
 #endif
